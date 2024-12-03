ae5ed1a8fba7b510619c71a33a2c428465eb9cbe
QSP <QSP@Desktop-Monitor-1st-Floor-Room3>
2024-12-03 16:11:29 +0530

    Added few lines

--- MVN/src/test/java/com/niqsam/testscript/AccountModule.java
+++ MVN/src/test/java/com/niqsam/testscript/AccountModule.java
@@ -1,11 +1,16 @@
 package com.niqsam.testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class AccountModule {
 	@Test
 public void createAccount() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
 	Reporter.log("createAccount",true);
 }
 }
